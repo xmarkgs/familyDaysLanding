c60bf9d16e397d906aca189de9f69d0d764a1dd9
Marko Zhyvotkov <mraner123@gmail.com>
2021-08-04 16:23:36 +0300

    Done: minor fixes

--- js/scripts.js
+++ js/scripts.js
@@ -3,23 +3,23 @@ $(document).ready(function () {
     $('.open-player-btn').fancybox({});
 
     // Masks
-    var input1 = document.getElementById('form-1-phone');
-    var maskOptions1 = {
-        mask: '+{38} (000)-000-00-00'
-    };
-    var mask1 = IMask(input1, maskOptions1);
+    // var input1 = document.getElementById('form-1-phone');
+    // var maskOptions1 = {
+    //     mask: '+{38} (000)-000-00-00'
+    // };
+    // var mask1 = IMask(input1, maskOptions1);
 
-    var input2 = document.getElementById('form-2-phone');
-    var maskOptions2 = {
-        mask: '+{38} (000)-000-00-00'
-    };
-    var mask2 = IMask(input2, maskOptions2);
+    // var input2 = document.getElementById('form-2-phone');
+    // var maskOptions2 = {
+    //     mask: '+{38} (000)-000-00-00'
+    // };
+    // var mask2 = IMask(input2, maskOptions2);
 
-    var input3 = document.getElementById('form-3-phone');
-    var maskOptions3 = {
-        mask: '+{38} (000)-000-00-00'
-    };
-    var mask3 = IMask(input3, maskOptions3);
+    // var input3 = document.getElementById('form-3-phone');
+    // var maskOptions3 = {
+    //     mask: '+{38} (000)-000-00-00'
+    // };
+    // var mask3 = IMask(input3, maskOptions3);
 
     // Form Submission
     $('#form_161570').ajaxSubmit({
